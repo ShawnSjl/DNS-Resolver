33cd74207e7476e2e8112c42328af162ae0d6d24
ShawnSjl <sjlshawn@gmail.com>
2026-04-27 20:56:58 -0400

    feat: implement main function for DNS server, add server initialization

---
+++ cmd/dns_server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/brown-cs1680-s26/final-jiale-xinran/internal/server/dns"
+)
+
+func main() {
+	// setup flags
+	dnsPortFlag := flag.String("port", "53", "port to listen DNS messages")
+	controllerPortFlag := flag.String("controller-port", "7878", "port to listen controller messages")
+	flag.Parse()
+
+	// validate port
+	dnsPort, err := strconv.Atoi(*dnsPortFlag)
+	if err != nil {
+		flag.Usage()
+		fmt.Printf("Error: port number is not valid\n")
+		os.Exit(1)
+	}
+	controllerPort, err := strconv.Atoi(*controllerPortFlag)
+	if err != nil {
+		flag.Usage()
+		fmt.Printf("Error: controller port number is not valid\n")
+		os.Exit(1)
+	}
+
+	// create root context
+	rootCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	dnsServer := dns.NewDNS(rootCtx)
+	dnsServer.Run(uint16(dnsPort))
+
+	// TODO: handle message from controller interface
+	log.Printf("Controller port: %d", controllerPort)
+	select {
+	case <-rootCtx.Done(): // wait for interrupt signal
+	}
+}
--- internal/server/dns/dns.go
+++ internal/server/dns/dns.go
@@ -2,6 +2,20 @@ package dns
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/miekg/dns"
+)
+
+const (
+	MTU            = 1500
+	UDPHeaderSize  = 8
+	IPv4HeaderSize = 20
+	IPv6HeaderSize = 40
+	UDP4MTU        = MTU - IPv4HeaderSize - UDPHeaderSize
+	UDP6MTU        = MTU - IPv6HeaderSize - UDPHeaderSize
 )
 
 type DNS struct {
@@ -24,4 +38,73 @@ type DNS struct {
 
 	// blocked domains
 	blocked *Blacklist
+}
+
+// ******************** Initialization Interface **********************
+
+func NewDNS(parent context.Context) *DNS {
+	ctx, cancel := context.WithCancel(parent)
+	return &DNS{
+		ctx:                  ctx,
+		cancel:               cancel,
+		sendInsideQueue:      NewSendInside(ctx),
+		receivedInsideQueue:  NewReceiveInside(ctx),
+		sendOutsideQueue:     NewSendOutside(ctx),
+		receivedOutsideQueue: NewReceiveOutside(ctx),
+		cache:                NewRecordCache(ctx),
+		questionTable:        NewQuestionTable(ctx),
+		blocked:              NewBlacklist(),
+	}
+}
+
+// ******************** Server **********************
+
+func (d *DNS) Run(port uint16) {
+	go func() {
+		// Create a IPV4 UDP socket listening on specified port
+		udpAddr, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("0.0.0.0:%d", port))
+		if err != nil {
+			log.Fatal("Fail to resolve UDP address: ", err)
+		}
+		conn, err := net.ListenUDP("udp4", udpAddr)
+		if err != nil {
+			log.Fatal("Fail to listen UDP: ", err)
+		}
+		defer func(conn *net.UDPConn) {
+			if closeErr := conn.Close(); closeErr != nil {
+				log.Println("Fail to close UDP connection: ", closeErr)
+			}
+		}(conn)
+
+		buffer := make([]byte, UDP4MTU)
+		for {
+			select {
+			case <-d.ctx.Done():
+				return
+
+			default:
+				// Read UDP packet from socket
+				n, addr, readErr := conn.ReadFrom(buffer)
+				if readErr != nil {
+					log.Println("Fail to read UDP: ", readErr)
+					continue
+				}
+
+				log.Printf("Received UDP from %s: \n", addr)
+
+				// Unpack the UDP packet into a DNS message
+				var msg dns.Msg
+				if unpackErr := msg.Unpack(buffer[:n]); unpackErr != nil {
+					log.Println("Fail to unpack UDP: ", unpackErr)
+					continue
+				}
+
+				log.Println("Received UDP from: ", msg.String())
+			}
+		}
+	}()
+}
+
+func (d *DNS) Terminate() {
+	d.cancel()
 }
