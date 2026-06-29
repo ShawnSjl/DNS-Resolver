11a228cae31eb094be60114f8b4ca808da520260
ShawnSjl <49027333+ShawnSjl@users.noreply.github.com>
2026-06-29 14:38:27 -0400

    Implement new controller and protocol (#1)
    * feat: use new repl for command handling
    
    * refactor: remove unused controller code
    
    * feat: implement logic of new protocol and repl commands
    
    * docs: use dnrd as new name for server

---
+++ cmd/dnr/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/ShawnSjl/DNS-Resolver/internal/cli"
+)
+
+func main() {
+	// Setup flags
+	addrFlag := flag.String("addr", "127.0.0.1", "address of the DNS resolver")
+	portFlag := flag.Int("port", 7878, "port of the DNS resolver")
+	flag.Parse()
+
+	// validate port
+	port := *portFlag
+	if port < 0 || port > 65535 {
+		flag.Usage()
+		log.Fatal("Invalid port number")
+	}
+
+	// Resolve address
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", *addrFlag, port))
+	if err != nil {
+		log.Fatal("Fail to resolve address: ", err)
+	}
+
+	// Connect to the DNS resolver
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal("Fail to dial address: ", err)
+	}
+	defer func(conn *net.TCPConn) {
+		_ = conn.Close()
+	}(conn)
+
+	// Start REPL
+	cli.REPL(conn)
+	os.Exit(0)
+}
